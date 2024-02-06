64ae0e6c6e40bdae7b087389c9a7006f057318e0
Edemit <shuldeshovwork013@gmail.com>
2024-02-06 20:36:11 +0100

    Several interface changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Blokus game is a strategy game for 2-4 players where you try to lay down as many of your tiles on the board as you can. When you play a tile, you must place it so it touches a corner on at least one of your other pieces. Once you play as many tiles as you can, count the tiles that you weren’t able to place to determine the winner.
 
 import tkinter 
+from tkinter import Scrollbar
 
 # Create the main window
 root = tkinter.Tk()
@@ -13,10 +14,17 @@ screen_height = root.winfo_screenheight()
 # Устанавливаем размеры окна в зависимости от размеров экрана
 root.geometry(f'{screen_width}x{screen_height}')
 
+# Делаем окно масштабируемым
+root.resizable(True, True)
+
 # Create the adaptive canvas
 global canvas
-canvas = tkinter.Canvas(root, width=screen_width, height=screen_height)
-canvas.pack()
+canvas_width = screen_width * 0.8  # Устанавливаем ширину холста равной 80% ширины экрана
+canvas_height = screen_height * 0.8  # Устанавливаем высоту холста равной 80% высоты экрана
+canvas = tkinter.Canvas(root, width=canvas_width, height=canvas_height, scrollregion=(0,0,canvas_width,canvas_height))
+
+# Размещаем холст в центре окна
+canvas.pack(expand=True)
 
 # Create the game field ganerator window in tkinter window before starting the game to change the size of the game field or number of players
 def gameFieldGenerator():
@@ -50,13 +58,16 @@ def gameFieldGenerator():
 
 # Create the game field
 def createGameField(size):
-    # Рассчитываем размеры линий в зависимости от выбранного размера
-    line_width = screen_width // size
-    line_height = screen_height // size
+    # Рассчитываем размеры клетки в зависимости от выбранного размера и размера холста
+    cell_size = min(canvas_width, canvas_height) // size
 
     for i in range(size):
-        canvas.create_line(i*line_width, 0, i*line_width, screen_height)
-        canvas.create_line(0, i*line_height, screen_width, i*line_height)
+        for j in range(size):
+            x1 = i * cell_size
+            y1 = j * cell_size
+            x2 = x1 + cell_size
+            y2 = y1 + cell_size
+            canvas.create_rectangle(x1, y1, x2, y2)
 
 def startGame():
 
@@ -64,8 +75,11 @@ def startGame():
     size = int(boardSize.get())
     players = int(numberOfPlayers.get())
     
-    # Закрываем окно генератора игрового поля
-    gameFieldWindow.destroy()
+    # Скрываем окно генератора игрового поля
+    gameFieldWindow.withdraw()
+
+    # Устанавливаем окно с полем поверх всех остальных окон
+    root.attributes('-topmost', True)
 
     # Здесь начинаем игру с выбранными параметрами
     # ...
